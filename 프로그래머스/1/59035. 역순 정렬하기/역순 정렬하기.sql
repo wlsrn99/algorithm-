6938c4d45cf52f1a4e94588aed253e668479a431
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-05-04 18:24:05 +0900

    [level 1] Title: 역순 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59035. 역순 정렬하기/역순 정렬하기.sql
@@ -0,0 +1,14 @@
+# 쿼리를 작성하는 목표, 확인할 지표 : 모든 동물의 이름과 보호 시작일 
+# 쿼리 계산 방법 : 
+# 데이터의 기간 : x
+# 사용할 테이블 : ANIMAL_INS 
+# Join KEY : x
+# 데이터 특징 : x
+
+SELECT
+ NAME,
+ DATETIME
+FROM 
+ ANIMAL_INS
+ORDER BY
+ ANIMAL_ID DESC
