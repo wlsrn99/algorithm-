401a06d37614ebd2fde9fcb001946ef365297b9b
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-05-04 18:36:38 +0900

    [level 1] Title: 동물의 아이디와 이름, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59403. 동물의 아이디와 이름/동물의 아이디와 이름.sql
@@ -0,0 +1,14 @@
+# 쿼리를 작성하는 목표, 확인할 지표 : 모든 동물의 아이디와 이름을 조회, ANIMAL_ID순 정렬
+# 쿼리 계산 방법 : 
+# 데이터의 기간 :
+# 사용할 테이블 :ANIMAL_INS
+# Join KEY :
+# 데이터 특징 :
+
+SELECT
+ ANIMAL_ID,
+ NAME
+FROM 
+ ANIMAL_INS
+ORDER BY
+ ANIMAL_ID
