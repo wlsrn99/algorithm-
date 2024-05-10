c531be652fbc83af33f5a03fc958cd5ca2a8f87a
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-05-10 12:49:06 +0900

    [level 1] Title: 모든 레코드 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59034. 모든 레코드 조회하기/모든 레코드 조회하기.sql
@@ -0,0 +1,13 @@
+# 쿼리를 작성하는 목표, 확인할 지표 :  모든 동물의 정보
+# 쿼리 계산 방법 :
+# 데이터의 기간 :
+# 사용할 테이블 :ANIMAL_INS
+# Join KEY :
+# 데이터 특징 :ANIMAL_ID순으로 조회
+
+SELECT
+ *
+FROM 
+ ANIMAL_INS
+ORDER BY
+ ANIMAL_ID
