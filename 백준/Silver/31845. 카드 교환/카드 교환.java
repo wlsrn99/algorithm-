cb3ef6057d98af7f99452d88c4f244921fa4b518
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-05-21 14:56:17 +0900

    [Silver III] Title: 카드 교환, Time: 144 ms, Memory: 14780 KB -BaekjoonHub

---
+++ 백준/Silver/31845. 카드 교환/카드 교환.java
@@ -0,0 +1,76 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int n = input[0];
+		int m = input[1];
+
+		int[] score = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		PriorityQueue<Card> pq = new PriorityQueue<>((o1, o2) -> o2.score - o1.score);
+		for (int i = 1; i <= n; i++) {
+			pq.offer(new Card(i, score[i - 1]));
+		}
+
+		Deque<Card> deque = new ArrayDeque<>();
+		while (!pq.isEmpty()) {
+			deque.offer(pq.poll());
+		}
+
+		int sum = 0;
+		boolean dummy = false;
+		int count = 0;
+		while (!deque.isEmpty()) {
+			if (deque.peekFirst().score >= 0 && deque.size() >= 2) {
+				// 가져오기
+				sum += deque.pollFirst().score;
+				// 제일 작은 값 주기
+				deque.pollLast();
+			} else {
+				if (deque.peekFirst().score >= 0) {
+					//가져오기
+					sum += deque.pollFirst().score;
+				} else {
+					// 더미 주고받으면서 턴을 계속 소비
+					break;
+				}
+			}
+
+			count++;
+			if (count == m) {
+				break;
+			}
+		}
+
+		System.out.println(sum);
+
+		/**
+		 * 점수가 양수일 때
+		 * 최대한 얻어야 함
+		 * 딜러의 패에서 해당 카드를 가져온다
+		 *
+		 * 점수가 음수일 때
+		 * 최대한 버려야 함
+		 */
+	}
+
+	private static class Card {
+		int num;
+		int score;
+
+		public Card(int num, int score) {
+			this.num = num;
+			this.score = score;
+		}
+	}
+}
