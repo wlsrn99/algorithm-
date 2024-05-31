148ee0e56496182bf5be9d659cf86c3312f1bab7
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-05-31 12:59:39 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 292 ms, Memory: 25448 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        br.readLine();//첫 입력 ENTER 버리기
+        Set<String> userNames = new HashSet<>();
+        int sum = 0;
+        for(int i = 0; i < N-1; i++){
+            String userName = br.readLine();
+            if(userName.equals("ENTER")){
+                sum += userNames.size();
+                userNames.clear();
+            }else {
+                userNames.add(userName);
+            }
+        }
+        sum += userNames.size();
+        System.out.println(sum);
+    }
+}
