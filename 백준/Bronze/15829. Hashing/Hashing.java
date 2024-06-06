d9263c4e53a9065313cdfb6c30bddf86074be52a
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-06-06 11:52:52 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14212 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,30 +1,25 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	/**
-	 *
-	 * 각 문자에 31의 거듭제곱 후 더하기
-	 * a의 아스키 값이 97, 각 문자에 96을 빼주면 주어진 숫자가 나온다
-	 *
-	 */
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		String input = br.readLine();
 
 		long sum = 0;
+		long power = 1;
+		final int MOD = 1234567891;
+		final int BASE = 31;
+
 		for (int i = 0; i < n; i++) {
 			char one = input.charAt(i);
-			long sequence = one - 96;
+			int value = one - 'a' + 1;
 
-			long square = (long)Math.pow(31, i);
-			sum += sequence * square;
+			sum = (sum + (value * power) % MOD) % MOD;
+			power = (power * BASE) % MOD;
 		}
 		System.out.println(sum);
-
 	}
 }
