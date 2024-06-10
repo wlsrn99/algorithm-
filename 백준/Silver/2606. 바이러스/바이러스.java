905667f4ff093425bcfc0de3b78afd5fee6e8774
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-06-10 10:57:16 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -4,53 +4,57 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
+/**
+ * 첫 시작한 노드의 간선과 연결되어 있는 모든 노드가 감염된다
+ * dfs,bfs아무거나 써도 될 것 같다
+ *
+ */
 public class Main {
-	static List<ArrayList<Integer>> nodeList;
+	static ArrayList<ArrayList<Integer>> nodeList = new ArrayList<>();
 	static boolean[] visited;
-
-	static int count;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int bridge = Integer.parseInt(br.readLine());
+		int node = Integer.parseInt(br.readLine());
+		int edge = Integer.parseInt(br.readLine());
 
-		visited = new boolean[n + 1];
-		nodeList = new ArrayList<>();
-		for (int i = 0; i < n + 1; i++) {
+		visited = new boolean[node + 1];
+		for (int i = 0; i < node + 1; i++) {
 			nodeList.add(new ArrayList<>());
 		}
 
-		for (int i = 0; i < bridge; i++) {
-			int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		for (int i = 0; i < edge; i++) {
+			String[] input = br.readLine().split(" ");
+			int node1 = Integer.parseInt(input[0]);
+			int node2 = Integer.parseInt(input[1]);
 
-			nodeList.get(input[0]).add(input[1]);
-			nodeList.get(input[1]).add(input[0]);
+			nodeList.get(node1).add(node2);
+			nodeList.get(node2).add(node1);
 		}
 
-		bfs();
+		int result = bfs(1);
+		System.out.println(result);
 
-		System.out.println(count);
 	}
 
-	private static void bfs() {
+	private static int bfs(int start) {
 		Queue<Integer> queue = new LinkedList<>();
-		queue.offer(1);
-		visited[1] = true;
+		queue.offer(start);
+		visited[start] = true;
+		int count = 0;
 
 		while (!queue.isEmpty()) {
-			for (int current : nodeList.get(queue.poll())) {
-				if (!visited[current]) {
-					visited[current] = true;
-					queue.offer(current);
+			for (int next : nodeList.get(queue.poll())) {
+				if (!visited[next]) {
+					queue.offer(next);
+					visited[next] = true;
 					count++;
 				}
 			}
 		}
+		return count;
 	}
 }
