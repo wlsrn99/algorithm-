ca7aa3e071f3f39b8993847c6bb017a39cf5dd9e
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-08-03 21:06:14 +0900

    [Silver V] Title: 영화감독 숌, Time: 260 ms, Memory: 85084 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 제일 작은 종말의 수는 666이고, 그 다음으로 큰 수는 1666, 2666, 3666, .... 이다
+ * 
+ */
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,9 +16,8 @@ public class Main {
 		int num = 666;
 
 		int count = 0;
-		boolean run = true;
 
-		while (run) {
+		while (true) {
 			String strNum = Integer.toString(num);
 			if (strNum.contains("666")) {
 				count++;
