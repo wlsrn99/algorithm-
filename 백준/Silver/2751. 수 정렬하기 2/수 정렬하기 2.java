94436fb2917019c10356e4cf866e4cc062ce60b1
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-08-06 21:04:36 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1836 ms, Memory: 148360 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -3,21 +3,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
+/**
+ * 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다
+ */
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 
-		Integer[] arr = new Integer[n];
+		Set<Integer> set = new TreeSet<>();
 		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			set.add(Integer.parseInt(br.readLine()));
 		}
 
-		Arrays.sort(arr);
 		StringBuilder sb = new StringBuilder();
-		for (Integer result : arr) {
+		for (int result : set) {
 			sb.append(result).append("\n");
 		}
 
