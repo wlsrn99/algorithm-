b673c9c38aeb6b5d91e9c8a79908c2ffbf079e10
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-08-08 21:11:56 +0900

    [Silver V] Title: 나이순 정렬, Time: 592 ms, Memory: 53548 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -4,21 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
+/**
+ * 회원들을 나이가 증가하는 순
+ * 나이가 같으면 먼저 가입한 사람이 앞에 오는 순서
+ */
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 
-		PriorityQueue<Member> pq = new PriorityQueue<>((o1, o2) -> {
-			if (o1.age != o2.age) {
-				return o1.age - o2.age;
-			} else {
+		Queue<Member> pq = new PriorityQueue<>((o1, o2) -> {
+			if (o1.age == o2.age) {
 				return o1.sequence - o2.sequence;
+			} else {
+				return o1.age - o2.age;
 			}
 		});
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i <= n; i++) {
 			String[] input = br.readLine().split(" ");
+
 			pq.add(new Member(Integer.parseInt(input[0]), input[1], i));
 		}
 
@@ -27,7 +33,6 @@ public class Main {
 			Member member = pq.poll();
 			sb.append(member.age).append(" ").append(member.name).append("\n");
 		}
-
 		System.out.println(sb);
 	}
 
