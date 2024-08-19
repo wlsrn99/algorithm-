de611b694a52f4f02733a6383d42e15cf0e5c3c6
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-08-19 21:06:39 +0900

    [Silver IV] Title: 제로, Time: 220 ms, Memory: 23828 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
+/**
+ * 재현이는 잘못된 수를 부를 때마다 0을 외쳐서, 가장 최근에 재민이가 쓴 수를 지우게 시킨다 -> 스택
+ * 이렇게 모든 수를 받아 적은 후 그 수의 합
+ */
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int k = Integer.parseInt(br.readLine());
-
 		Stack<Integer> stack = new Stack<>();
 		for (int i = 0; i < k; i++) {
 			int input = Integer.parseInt(br.readLine());
@@ -19,8 +22,8 @@ public class Main {
 			} else {
 				stack.push(input);
 			}
+			
 		}
-
 		int sum = 0;
 		while (!stack.isEmpty()) {
 			sum += stack.pop();
