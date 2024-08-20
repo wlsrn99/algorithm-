871ec43a9f67118b7047a72e21891d07d89f0cd9
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-08-20 21:29:40 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 984 ms, Memory: 163784 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,32 +7,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String sangGeunCard = br.readLine();
-        int M = Integer.parseInt(br.readLine());
-        String cardType = br.readLine();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String[] nCard = br.readLine().split(" ");
+		int M = Integer.parseInt(br.readLine());
+		String[] mCard = br.readLine().split(" ");
 
-        String[] sgc = sangGeunCard.split(" ");
-        Map <String, Integer>sangGeunCardType = new HashMap<>();
-        for(int i = 0; i < N; i++){
-            sangGeunCardType.put(sgc[i],sangGeunCardType.getOrDefault(sgc[i],0)+1);
-        }
+		Map<String, Integer> map = new HashMap<>();
+		for (int i = 0; i < N; i++) {
+			map.put(nCard[i], map.getOrDefault(nCard[i], 0) + 1);
+		}
 
-        StringBuilder sb = new StringBuilder();
-        String[] cardTypeKeys = cardType.split(" ");
+		StringBuilder sb = new StringBuilder();
+		for (String key : mCard) {
+			Integer sangGeunCardValue = map.get(key);
 
-        for(String key : cardTypeKeys){
-            Integer sangGeunCardValue = sangGeunCardType.get(key);
+			if (sangGeunCardValue != null) {
+				sb.append(sangGeunCardValue).append(" ");
+			} else {
+				sb.append(0).append(" ");
+			}
+		}
 
-            if(sangGeunCardValue != null) {
-                sb.append(sangGeunCardValue).append(" ");
-            }else{
-                sb.append(0).append(" ");
-            }
-        }
-
-        System.out.println(sb);
-    }
+		System.out.println(sb);
+	}
 }
