32dc184f53b7a7424fc8560a4d693faf2b7acad2
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-08-23 21:22:30 +0900

    [Silver III] Title: 프린터 큐, Time: 128 ms, Memory: 15012 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -4,50 +4,66 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 
+/**
+ * 현재 Queue의 가장 앞에 있는 문서의 ‘중요도’를 확인
+ * 나머지 문서들 중 현재 문서보다 중요도가 높은 문서가 하나라도 있다면, 이 문서를 인쇄하지 않고 Queue의 가장 뒤에 재배치
+ *
+ * 뒤에 재배치 -> 덱 활용
+ */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testNum = Integer.parseInt(br.readLine());
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 
-        for (int i = 0; i < testNum; i++) {
-            String[] input = br.readLine().split(" ");
-            int N = Integer.parseInt(input[0]);
-            int M = Integer.parseInt(input[1]);
+		int t = Integer.parseInt(br.readLine());
 
-            int[] priorities = new int[N]; // 문서의 중요도를 저장할 배열
-            Deque<Integer> indexes = new ArrayDeque<>(); // 문서의 인덱스를 저장할 Deque
-            String[] priorityStr = br.readLine().split(" ");
-            for (int j = 0; j < N; j++) {
-                int priority = Integer.parseInt(priorityStr[j]);
-                priorities[j] = priority;
-                indexes.add(j); // 인덱스를 Deque에 추가
-            }
+		for (int i = 0; i < t; i++) {
+			int[] input = Arrays.stream(br.readLine().split(" "))
+				.mapToInt(Integer::parseInt).toArray();
 
-            int count = 0;
-            while (!indexes.isEmpty()) {
-                int currentIndex = indexes.poll(); // 현재 문서의 인덱스를 가져옴
-                int currentPriority = priorities[currentIndex]; // 현재 문서의 중요도를 가져옴
+			int n = input[0]; // 문서의 개수
+			int m = input[1]; // 몇 번째로 인쇄되었는지 궁금한 문서의 인덱스
 
-                boolean isHighestPriority = true;
-                for (int index : indexes) {
-                    if (priorities[index] > currentPriority) {
-                        isHighestPriority = false;
-                        break;
-                    }
-                }
+			int[] document = Arrays.stream(br.readLine().split(" "))
+				.mapToInt(Integer::parseInt).toArray();
 
-                if (isHighestPriority) {
-                    count++;
-                    if (currentIndex == M) {
-                        System.out.println(count);
-                        break;
-                    }
-                } else {
-                    indexes.add(currentIndex); // 다시 Deque에 추가하여 순서를 유지
-                }
-            }
-        }
-    }
+			Deque<Integer> deque = new ArrayDeque<>();
+			for (int j = 0; j < n; j++) {
+				deque.offerLast(j); //문서의 인덱스를 저장
+			}
+			
+			// 프린터기 실행
+			doPrinter(deque, document, m);
+		}
+		System.out.println(sb);
+	}
+
+	private static void doPrinter(Deque<Integer> deque, int[] document, int m) {
+		int count = 0;
+		while (!deque.isEmpty()) {
+			int current = deque.pollFirst();
+			boolean check = false;
+
+			for (int idx : deque) {
+				if (document[current] < document[idx]) {
+					check = true;
+					deque.offerLast(current);
+					break;
+				}
+			}
+
+			if (!check) {
+				// 인쇄 순서 증가
+				count++;
+				if (current == m) {
+					sb.append(count).append("\n");
+					break;
+				}
+			}
+		}
+	}
 }
