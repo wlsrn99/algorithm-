e1b1e3ab4b57e756e219e5c057e3a8434676db6c
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-08-26 21:39:30 +0900

    [Silver II] Title: 랜선 자르기, Time: 164 ms, Memory: 17824 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,46 +5,44 @@ import java.util.Arrays;
 
 public class Main {
 
-	static long[] arr;
-	static int k, n;
+    static long[] arr;
+    static int k, n;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		k = input[0];
-		n = input[1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        k = input[0];
+        n = input[1];
 
-		arr = new long[k];
-		for (int i = 0; i < k; i++) {
-			arr[i] = Long.parseLong(br.readLine());
-		}
+        arr = new long[k];
+        for (int i = 0; i < k; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+        }
 
-		Arrays.sort(arr);
+        Arrays.sort(arr);
 
-		long start = 1;
-		long end = arr[k - 1];
-		long result = 0;
+        long start = 0;  // 0부터 시작
+        long end = arr[k - 1] + 1;  // 상한을 위해 +1
 
-		while (start <= end) {
-			long mid = start + (end - start) / 2; // 오버플로우 방지
+        while (start < end) {
+            long mid = start + (end - start) / 2;
 
-			if (check(mid)) {
-				result = mid;
-				start = mid + 1;
-			} else {
-				end = mid - 1;
-			}
-		}
+            if (check(mid)) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
 
-		System.out.println(result);
-	}
+        System.out.println(start - 1);  // 결과는 상한 - 1
+    }
 
-	private static boolean check(long mid) {
-		long sum = 0;
-		for (long one : arr) {
-			sum += one / mid;
-		}
-
-		return sum >= n; // 조건 변경
-	}
+    private static boolean check(long mid) {
+        if (mid == 0) return true;  // 0으로 나누는 것 방지
+        long sum = 0;
+        for (long one : arr) {
+            sum += one / mid;
+        }
+        return sum >= n;
+    }
 }
