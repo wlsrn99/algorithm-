8259ae31109a9b0b15730e6a482516af721b015d
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-09-04 21:15:54 +0900

    [Silver III] Title: 피보나치 함수, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -4,6 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * fibonacci(1)과 fibonacci(0)이 출력되는 횟수
+ * 
+ * 0 -> f(0) 
+ * 1 -> f(1)
+ * 2 -> f(1) + f(0) / 1 1
+ * 3 -> f(2) + f(1) = 2f(1) + f(0) / 2 1
+ * 4 -> f(3) + f(2) = 3f(1) + 2f(0)
+ * 5 -> f(4) + f(3) = 5f(1) + 3f(0)
+ */
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -13,23 +23,16 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < t; i++) {
 			int n = Integer.parseInt(br.readLine());
-			/**
-			 * 0 -> 0이 1
-			 * 1 -> 1이 1
-			 * 2 -> 1 + 0
-			 * 3 -> 2 + 1
-			 * 4 -> 3 + 2
-			 * 5 -> 4 + 3
-			 */
+
 			if (n == 0) {
 				sb.append(1).append(" ").append(0).append("\n");
 			} else if (n == 1) {
 				sb.append(0).append(" ").append(1).append("\n");
 			} else {
 				int[] dp0 = new int[n + 1];
-				dp0[0] = 1;
+				dp0[0] = 1; // fibonacci(0)
 				int[] dp1 = new int[n + 1];
-				dp1[1] = 1;
+				dp1[1] = 1; // fibonacci(1)
 
 				for (int j = 2; j <= n; j++) {
 					dp0[j] = dp0[j - 1] + dp0[j - 2];
