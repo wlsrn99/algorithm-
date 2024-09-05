78dad5580bf4e93f6292b0151497e5d5b4e09277
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-09-05 21:29:40 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18188 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -4,22 +4,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 연산을 사용하는 횟수의 최솟값
+ * 시간 0.15초 -> dp
+ *
+ * arr[n+1] 배열을 생성, arr[0]은 사용하지 않음
+ * 최솟값
+ *
+ * arr[1] = 1
+ * arr[2] = 1
+ * arr[3] = 1
+ *
+ * x > 3
+ * arr[4] = arr[1] + arr[3] -> arr[x] = arr[x-3] + arr[x-1]
+ * arr[5] = arr[2] + arr[3] -> arr[x] = arr[x-3] + arr[x-2]
+ * arr[6] = arr[3] + arr[3] -> arr[x] = arr[x-3] + arr[x-3]
+ *
+ * arr[7] = arr[1] + arr[6] -> arr[x] = arr[x-6] + arr[x-1]
+ * arr[8] = arr[2] + arr[6] -> arr[x] = arr[x-6] + arr[x-2]
+ *
+ * ---------------------------------------------------------
+ * 잘못생각함
+ *
+ * 1을 빼는 연산을 전부 수행시키기
+ * 2로 나누어 떨어질 때 dp[x/2] + 1
+ * 3으로 나누어 떨어질 때 dp[x/3] + 1
+ *
+ */
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
-		
-		if( n == 1){
+		if (n == 1) { //1일때는 수행횟수 0
 			System.out.println(0);
-		} else if (n < 4) {
+		} else if (n == 2 || n == 3) {
 			System.out.println(1);
 		} else {
 			int[] dp = new int[n + 1];
 			dp[2] = 1;
 			dp[3] = 1;
 			for (int i = 2; i < n + 1; i++) {
-				dp[i] = dp[i - 1] + 1;
+				dp[i] = dp[i - 1] + 1; // 1을 빼는 경우 전부 수행
 
 				if (i % 2 == 0) {
 					dp[i] = Math.min(dp[i], dp[i / 2] + 1);
@@ -34,3 +60,4 @@ public class Main {
 		}
 	}
 }
+
