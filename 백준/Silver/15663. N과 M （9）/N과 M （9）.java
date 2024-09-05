ccab9608b5d05a18d6cc747c6975a9165df38adf
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-09-05 13:36:54 +0900

    [Silver II] Title: N과 M (9), Time: 176 ms, Memory: 19000 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -6,48 +6,47 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    static boolean[] visited;
-    static StringBuilder sb = new StringBuilder();
-    static int n, m;
-    static int[] arr;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+	static int n, m;
+	static int[] arr;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        n = input[0]; // n개의 자연수
-        m = input[1]; // m개를 고른 수열
-        visited = new boolean[n];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		n = input[0]; // n개의 자연수
+		m = input[1]; // m개를 고른 수열
+		visited = new boolean[n];
 
-        arr = Arrays.stream(br.readLine().split(" "))
-            .mapToInt(Integer::parseInt)
-            .sorted()
-            .toArray();
+		arr = Arrays.stream(br.readLine().split(" "))
+			.mapToInt(Integer::parseInt)
+			.sorted()
+			.toArray();
 
-        int[] result = new int[m];
-        calculate(result, 0);
-        System.out.println(sb);
-    }
+		int[] result = new int[m];
+		calculate(result, 0);
+		System.out.println(sb);
+	}
 
-    private static void calculate(int[] result, int count) {
-        if (count == m) {
-            for (int i : result) {
-                sb.append(i).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-        int prev = 0; // 이전에 사용한 숫자를 기억
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                if (i > 0 && arr[i] == arr[i-1] && !visited[i-1]) {
-                    continue; // 같은 레벨에서 이전 숫자와 같고, 이전 숫자를 사용하지 않았다면 스킵
-                }
-                visited[i] = true;
-                result[count] = arr[i];
-                calculate(result, count + 1);
-                visited[i] = false;
-                prev = arr[i]; // 현재 숫자를 이전 숫자로 기억
-            }
-        }
-    }
+	private static void calculate(int[] result, int count) {
+		if (count == m) {
+			for (int i : result) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (!visited[i]) {
+				if (i > 0 && arr[i] == arr[i - 1] && !visited[i - 1]) {
+					continue; // 같은 레벨에서 이전 숫자와 같고, 이전 숫자를 사용하지 않았다면 스킵
+				}
+				visited[i] = true;
+				result[count] = arr[i];
+				calculate(result, count + 1);
+				visited[i] = false;
+			}
+		}
+	}
 }
