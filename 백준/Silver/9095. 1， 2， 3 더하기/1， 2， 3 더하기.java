e24acd945f2ab167a5da46168187b1ccea89484b
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-09-09 21:15:07 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -4,34 +4,72 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * n을 1, 2, 3의 합으로 나타내는 방법의 수
+ * 1 -> 1가지
+ * 2 -> 2가지
+ * 3 -> 1 + 1 + 1
+ *      1 + 2
+ *      2 + 1
+ *      3
+ *      4가지
+ *
+ * 4 -> 1 + 1 + 1 + 1
+ *      1 + 1 + 2
+ *      1 + 2 + 1
+ *      1 + 3
+ *      2 + 1 + 1
+ *      3 + 1
+ *      1+3
+ *      7가지
+ *
+ * 5 -> 1 + 1 + 1 + 1 + 1
+ *      1 + 1 + 1 + 2
+ *      1 + 1 + 2 + 1
+ *      1 + 2 + 1 + 1
+ *      2 + 1 + 1 + 1
+ *      1 + 2 + 2
+ *      2 + 1 + 2
+ *      2 + 2 + 1
+ *      1 + 1 + 3
+ *      1 + 3 + 1
+ *      3 + 1 + 1
+ *      2 + 3
+ *      3 + 2
+ *
+ *      13가지
+ *
+ *  i = [i - 1] + [i - 2] + [i - 3]
+ *
+ */
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int t = Integer.parseInt(br.readLine());
-
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < t; i++) {
 			int n = Integer.parseInt(br.readLine());
-
 			if (n == 1) {
-				System.out.println(1);
+				sb.append("1").append("\n");
 			} else if (n == 2) {
-				System.out.println(2);
+				sb.append("2").append("\n");
 			} else if (n == 3) {
-				System.out.println(4);
+				sb.append("4").append("\n");
 			} else {
-				int[] arr = new int[n + 1];
-				arr[1] = 1;
-				arr[2] = 2;
-				arr[3] = 4;
+				int[] dp = new int[n + 1];
+				dp[1] = 1;
+				dp[2] = 2;
+				dp[3] = 4;
 
 				for (int j = 4; j < n + 1; j++) {
-					arr[j] = arr[j - 3] + arr[j - 2] + arr[j - 1];
+					dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
 				}
 
-				System.out.println(arr[n]);
+				sb.append(dp[n]).append("\n");
 			}
-
 		}
+		System.out.println(sb);
 	}
 }
+
