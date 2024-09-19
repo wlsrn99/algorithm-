5e487749b0538abe1333525d3346ce4d9a3af3fb
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-09-19 21:19:12 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 108 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -3,40 +3,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
+/**
+ * 괄호를 적절히 쳐서 이 식의 값을 최소
+ * -> -가 나오면 다음 -가 나올때 까지 괄호
+ */
 public class Main {
-	public static void main(String[] args) throws IOException {
-		/**
-		 * -가 나오면 다음 -가 나올때 까지 괄호
-		 */
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split("-");
-		List<String> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split("-");
 
-		for (String s : input) {
-			list.add(s);
-		}
+        long result = 0;
+        for (int i = 0; i < input.length; i++) {
+            int[] nums = Arrays.stream(input[i].split("\\+"))
+                .mapToInt(Integer::parseInt).toArray();
 
-		int size = list.size();
+            long sum = 0;
+            for (int num : nums) {
+                sum += num;
+            }
 
-		long sum = 0;
-		for (int i = 0; i < size; i++) {
-			int[] numArr = Arrays.stream(list.get(i).split("\\+")).mapToInt(Integer::parseInt).toArray();
-
-			if (i == 0) {
-				for (int num : numArr) {
-					sum += num;
-				}
-			} else {
-				for (int num : numArr) {
-					sum -= num;
-				}
-			}
-		}
-
-		System.out.println(sum);
-	}
+            if (i == 0) {
+                result += sum;
+            } else {
+                result -= sum;
+            }
+        }
+        System.out.println(result);
+    }
 }
