9fd561396569b9bd5520ae9fc5b247b5d3008960
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-10-02 21:14:52 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 18696 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -3,23 +3,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/*
-X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동
-순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동
+/**
+ * X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동
+ * 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동
  */
+
 public class Main {
-	static int max = 100001;
-	static boolean[] visited = new boolean[max];
+	static int MAX = 100001;
+	static boolean[] visited = new boolean[MAX];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split(" ");
+		int[] input = Arrays.stream(br.readLine().split(" "))
+			.mapToInt(Integer::parseInt).toArray();
 
-		int n = Integer.parseInt(input[0]);
-		int k = Integer.parseInt(input[1]);
+		int n = input[0]; //수빈
+		int k = input[1]; //동생
 
 		System.out.println(bfs(n, k));
 	}
@@ -33,8 +36,7 @@ public class Main {
 
 		while (!queue.isEmpty()) {
 			int size = queue.size();
-
-			for (int i = 0; i < size; i++) {
+			for (int i = 0; i < size; i++) { //현재 큐에 있는 요소들을 전부 확인하기
 				int current = queue.poll();
 
 				if (current == k) {
@@ -47,17 +49,16 @@ public class Main {
 					visited[current - 1] = true;
 				}
 				// X+1로 이동하는 경우
-				if (current + 1 < max && !visited[current + 1]) {
+				if (current + 1 < MAX && !visited[current + 1]) {
 					queue.offer(current + 1);
 					visited[current + 1] = true;
 				}
 				// X*2로 이동하는 경우
-				if (current * 2 < max && !visited[current * 2]) {
+				if (current * 2 < MAX && !visited[current * 2]) {
 					queue.offer(current * 2);
 					visited[current * 2] = true;
 				}
 			}
-
 			time++;
 		}
 
