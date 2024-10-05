13020d102f0bf905001b8c0421c359edba976854
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-10-05 21:20:21 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14696 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,71 +1,75 @@
 
-/**
- *
- * 단지내 집의 수를 오름차순으로 정렬
- */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+/**
+ * 1은 집이 있는 곳을, 0은 집이 없는 곳
+ * 연결된 집의 모임
+ *
+ * 연결되었다는 것은 어떤 집이 좌우, 혹은 아래위로 다른 집이 있는 경우
+ *
+ * 첫 번째 줄에는 총 단지수
+ * 단지내 집의 수를 오름차순으로 정렬
+ */
+
 public class Main {
-	static boolean[][] visited;
-	static int n;
 	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
+	static int[] dy = {0, -1, 0, 1};
+	static int n;
+	static int[][] arr;
+	static boolean[][] visited;
+	static int total = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		visited = new boolean[n][n];
+		arr = new int[n][n];
 		for (int i = 0; i < n; i++) {
-			String[] input = br.readLine().split("");
-			for (int j = 0; j < n; j++) {
-				if (input[j].equals("0")) {
-					visited[i][j] = true;
-				}
-			}
+			arr[i] = Arrays.stream(br.readLine().split(""))
+				.mapToInt(Integer::parseInt).toArray();
 		}
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
+
 		StringBuilder sb = new StringBuilder();
-		int count = 0;
+		PriorityQueue pq = new PriorityQueue<>();
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (!visited[i][j]) {
-					count++;
-					pq.add(bfs(i, j));
+				if (visited[i][j] == false && arr[i][j] == 1) {
+					pq.offer(bfs(i, j));
 				}
 			}
 		}
-		sb.append(count).append("\n");
+		sb.append(total).append("\n");
 		while (!pq.isEmpty()) {
 			sb.append(pq.poll()).append("\n");
 		}
-
 		System.out.println(sb);
-
 	}
 
-	private static int bfs(int i, int j) {
+	private static int bfs(int x, int y) {
 		Queue<Coordinate> queue = new LinkedList<>();
-		queue.add(new Coordinate(i, j));
-		visited[i][j] = true;
+		queue.add(new Coordinate(x, y));
+		visited[x][y] = true;
 		int count = 0;
 		while (!queue.isEmpty()) {
+			Coordinate current = queue.poll();
 			count++;
-			Coordinate coordinate = queue.poll();
-			for (int k = 0; k < 4; k++) {
-				int nx = coordinate.x + dx[k];
-				int ny = coordinate.y + dy[k];
-				if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny]) {
+			for (int i = 0; i < 4; i++) {
+				int nx = current.x + dx[i];
+				int ny = current.y + dy[i];
+				if (nx >= 0 && ny >= 0 && nx < n && ny < n && !visited[nx][ny] && arr[nx][ny] == 1) {
 					visited[nx][ny] = true;
 					queue.add(new Coordinate(nx, ny));
 				}
 			}
 		}
+		total++;
 		return count;
 	}
 
@@ -78,4 +82,5 @@ public class Main {
 			this.y = y;
 		}
 	}
+
 }
