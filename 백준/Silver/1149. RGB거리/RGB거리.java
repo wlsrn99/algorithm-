28eab39550e560f041132d75ab8e4f7374dddb75
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-11-01 21:27:12 +0900

    [Silver I] Title: RGB거리, Time: 132 ms, Memory: 15772 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -5,52 +5,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-/*
-# 요구사항
-시간제한:0.5초 , 메모리 128로 다 작아서 효율적인 프로그래밍 필요
-RGB거리에는 집이 N개 있다
-거리는 선분으로 나타낼 수 있고, 1번 집부터 N번 집이 순서대로 있다
-아래 규칙을 만족하면서 모든 집을 칠하는 비용의 최솟값
-1. N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
-2. i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
-# 내가 생각한 논리
-
-memo[i][j]는 i번째 집을 j색으로 칠할 때의 최소 비용이라고 생각하면
--> 각 색상별로 i번째 집을 칠할 때의 최소 비용은
--> 이전 집까지의 다른 색을 칠했을 때 최소 비용과 현재 집을 해당 색으로 칠하는 비용을 더한 값
-
-N-1까지 도달했을 때, 이 중에서 가장 작은 값
-
+/**
+ * 0.5 초 (추가 시간 없음) -> dp
+ *
+ * 1번 집의 색은 2번 집의 색과 같지 않아야 한다.
+ * N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+ * i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다
  */
 public class Main {
-    static int[] memo = new int[3]; // 비용을 저장하는 배열
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[][] cost;
+	static int[][] dp;
 
-        int[][] arr = new int[N][3];
-        for(int i = 0; i < N; i++){
-            String[] input = br.readLine().split(" ");
-            for(int j = 0; j < 3; j++){
-                arr[i][j] = Integer.parseInt(input[j]);
-            }
-        }
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+		cost = new int[n][3];
+		dp = new int[n][3];
 
-        int[][] memo = new int[N][3];
-        memo[0][0] = arr[0][0];
-        memo[0][1] = arr[0][1];
-        memo[0][2] = arr[0][2];
+		for (int i = 0; i < n; i++) {
+			cost[i] = Arrays.stream(br.readLine().split(" "))
+				.mapToInt(Integer::parseInt).toArray();
+		}
 
-        for(int i = 1; i < N; i++){
-            // 현재 색깔을 고르고 칠했을 때의 최소 비용은
-            // 이전에 다른 색깔을 골랐을 때의 최소 비용에 현재 색깔을 칠했을 때다
-            memo[i][0] = Math.min(memo[i - 1][1], memo[i - 1][2]) + arr[i][0];
-            memo[i][1] = Math.min(memo[i - 1][0], memo[i - 1][2]) + arr[i][1];
-            memo[i][2] = Math.min(memo[i - 1][1], memo[i - 1][0]) + arr[i][2];
-        }
-
-
-        System.out.println(Arrays.stream(memo[N-1]).min().getAsInt());
-    }
-
+		dp[0][0] = cost[0][0];
+		dp[0][1] = cost[0][1];
+		dp[0][2] = cost[0][2];
+		for (int i = 1; i < n; i++) {
+			dp[i][0] = cost[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+			dp[i][1] = cost[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+			dp[i][2] = cost[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+		}
+		
+		int min = Arrays.stream(dp[n - 1]).min().getAsInt();
+		System.out.println(min);
+	}
 }
