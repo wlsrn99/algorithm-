518a802ea835c5d49bab052d6bc917042a152848
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-11-04 21:21:09 +0900

    [Silver I] Title: 정수 삼각형, Time: 284 ms, Memory: 31100 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,47 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 맨 위층 7부터 시작해서 아래에 있는 수 중 하나를 선택하여 아래층으로 내려올 때,
+ * 이제까지 선택된 수의 합이 최대가 되는 경로
+ *
+ * 아래층에 있는 수는 현재 층에서 선택된 수의 대각선 왼쪽 또는 대각선 오른쪽에 있는 것 중에서만 선택
+ *
+ * 7, 3,8 8,1,0
+ */
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[][] triangle;
+	static int[][] dp;
+
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+		triangle = new int[n][n];
+		dp = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			triangle[i] = Arrays.stream(br.readLine().split(" "))
+				.mapToInt(Integer::parseInt).toArray();
+		}
+		dp[n - 1] = triangle[n - 1];
+		
+		// n(1 ≤ n ≤ 500)
+		// n이 1일때 
+		if (n == 1){
+			System.out.println(triangle[0][0]);
+		}else {
+			for (int i = n - 2; i >= 1; i--) {
+				for (int j = 0; j <= i; j++) {
+					dp[i][j] = triangle[i][j] + Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
+				}
+			}
+			int max = Math.max(dp[1][0], dp[1][1]);
+			int result = max + triangle[0][0];
+			System.out.println(result);
+		}
+		
+	}
+}
