cbb923588cf413d8563a165754a4ff2ae98bbf6d
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-11-04 21:26:44 +0900

    [Silver I] Title: 정수 삼각형, Time: 284 ms, Memory: 30348 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -11,7 +11,8 @@ import java.util.Arrays;
  *
  * 아래층에 있는 수는 현재 층에서 선택된 수의 대각선 왼쪽 또는 대각선 오른쪽에 있는 것 중에서만 선택
  *
- * 7, 3,8 8,1,0
+ * 맨 아래 0번째 인덱스부터 시작해서 2개씩 비교한 뒤 큰 값을 위에있는 배열에 저장
+ *
  */
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,21 +28,19 @@ public class Main {
 				.mapToInt(Integer::parseInt).toArray();
 		}
 		dp[n - 1] = triangle[n - 1];
-		
+
 		// n(1 ≤ n ≤ 500)
-		// n이 1일때 
-		if (n == 1){
+		if (n == 1) { // n이 1일때 예외
 			System.out.println(triangle[0][0]);
-		}else {
-			for (int i = n - 2; i >= 1; i--) {
+		} else {
+			for (int i = n - 2; i >= 0; i--) {
 				for (int j = 0; j <= i; j++) {
 					dp[i][j] = triangle[i][j] + Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
 				}
 			}
-			int max = Math.max(dp[1][0], dp[1][1]);
-			int result = max + triangle[0][0];
-			System.out.println(result);
+			// int max = Math.max(dp[1][0], dp[1][1]);
+			System.out.println(dp[0][0]);
 		}
-		
+
 	}
 }
