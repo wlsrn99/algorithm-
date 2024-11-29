1b4c8266f9d2ee668a4a9242cf71d72cc7b3f001
wlsrn99 <131749035+wlsrn99@users.noreply.github.com>
2024-11-29 21:12:05 +0900

    [Silver III] Title: 등수 매기기, Time: 584 ms, Memory: 54952 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.java
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -3,35 +3,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
- *  불만도의 합을 최소로 하는 프로그램 (|A - B|)
- *  각 사람이 제출한 예상 등수를 바탕으로 임의로 등수, 동석차 없이
- *  1 5 3 1 2
- *
- *  1 1 2 3 5 
- *  1 2 3 4 5 
- *
- *  정렬 후 i + 1와 비교, i + 1과 같지 않다면 불만도 합하기
+ *  A등으로 예상하였는데 실제 등수가 B등이 될 경우, 이 사람의 불만도는 A와 B의 차이 (|A - B|)로 수치화
+ *  각 사람의 예상 등수가 주어졌을 때, 김 조교를 도와 이러한 불만도의 합을 최소로 하는 프로그램
  */
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Queue<Integer> pq = new PriorityQueue<>();
 		int n = Integer.parseInt(br.readLine());
-
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			pq.add(Integer.parseInt(br.readLine()));
 		}
 
-		Arrays.sort(arr);
-		long sum = 0;
-		for (int i = 0; i < n; i++) {
-			if (arr[i] != i + 1) {
-				sum += Math.abs(arr[i] - (i + 1));
-			}
+		// 500,000 x 500,000 = 250,000,000,000
+		// long
+		long result = 0;
+		for (int rank = 1; rank <= n; rank++) {
+			int expectation = pq.poll();
+			int current = Math.abs(rank - expectation);
+
+			result += current;
 		}
-		System.out.println(sum);
+		System.out.println(result);
 	}
 }
